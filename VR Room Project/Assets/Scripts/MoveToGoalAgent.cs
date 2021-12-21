a1db7c56c5a634bbe01792dc8d66b85cc292bc18
NabilEMAP <56017782+NabilEMAP@users.noreply.github.com>
2021-12-21 10:50:45 +0100

    Added Playermovement.cs, XR Rig and Interaction Manager

--- VR Room Project/Assets/Scripts/MoveToGoalAgent.cs
+++ VR Room Project/Assets/Scripts/MoveToGoalAgent.cs
@@ -7,13 +7,17 @@ using UnityEngine;
 
 public class MoveToGoalAgent : Agent
 {
+    public float moveSpeed = 1f;
     public override void CollectObservations(VectorSensor sensor)
     {
         sensor.AddObservation(transform.position);
     }
     public override void OnActionReceived(ActionBuffers actions)
     {
-        float move
-        Debug.Log(actions.ContinuousActions[0]);
+        float move = actions.ContinuousActions[0];
+        float rotateY_Direction = actions.ContinuousActions[1];
+        float rotateX_Tilt = actions.ContinuousActions[2];
+
+        transform.position += new Vector3(move, 0, 0) * Time.deltaTime * moveSpeed;
     }
 }
